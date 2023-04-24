44e5c95fe3617f7bec11b939c41e5b707eed8354
AlexSoft <4276038@gmail.com>
2023-04-24 22:01:31 +0300

    task_a added

---
+++ task_a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int caseCount = int.Parse(Console.ReadLine()!);
+        int [] array = new int[caseCount];
+        for (var i = 0; i < caseCount; i++)
+        {
+            array[i]= Console.ReadLine()!.Split(' ').Select(item => int.Parse(item)).ToArray().Sum();
+        }
+        foreach (var item in array)
+        {
+           Console.WriteLine(item); 
+        }
+    }
+}
