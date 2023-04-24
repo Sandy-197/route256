cfced572d4dee2a486a65bcc5501ed7a189bc62f
AlexSoft <4276038@gmail.com>
2023-04-24 22:08:59 +0300

    modifi Program.cs

--- task_a/Program.cs
+++ task_a/Program.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Linq;
-
-public class Program
+﻿public class Program
 {
     public static void Main(string[] args)
     {
         int caseCount = int.Parse(Console.ReadLine()!);
-        int [] array = new int[caseCount];
+        int[] array = new int[caseCount];
         for (var i = 0; i < caseCount; i++)
         {
-            array[i]= Console.ReadLine()!.Split(' ').Select(item => int.Parse(item)).ToArray().Sum();
+            array[i] = Console.ReadLine()!.Split(' ').Select(item => int.Parse(item)).ToArray().Sum();
         }
         foreach (var item in array)
         {
-           Console.WriteLine(item); 
+            Console.WriteLine(item);
         }
     }
 }
